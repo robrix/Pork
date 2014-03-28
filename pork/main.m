4956cbb9994734bcf38339a2d5ea2df86ef75328
Rob Rix <rob.rix@github.com>
2014-03-28 09:05:48 -0400

    Initial Commit

---
+++ pork/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pork
+//
+//  Created by Rob Rix on 2014-03-28.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
