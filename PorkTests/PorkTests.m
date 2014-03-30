471b221f3b491b431c1327f494ef4f5a451dc6dc
Rob Rix <rob.rix@github.com>
2014-03-30 02:21:45 -0400

    Update PorkTests & Lagrangian & Reducers.

--- PorkTests/PorkTests.m
+++ PorkTests/PorkTests.m
@@ -1,2 +1,12 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 //  Stub so that the test bundle builds.
+@interface PORKTests : XCTestCase
+@end
+
+@implementation PORKTests
+
++(id)defaultTestSuite {
+	return [L3TestSuite suiteForExecutablePath:@L3_BUNDLE_LOADER];
+}
+
+@end
