99c437669e8845abfa571ac1b1274a65b4a648f8
Rob Rix <rob.rix@github.com>
2015-05-14 09:57:49 -0400

    Readjoin the diacriticals.

--- Pork/Diacriticals.swift
+++ Pork/Diacriticals.swift
@@ -1,6 +1,15 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public func readjoinDecomposedDiacriticals(string: NSAttributedString) -> NSAttributedString {
+	let string = string.mutableCopy() as! NSMutableAttributedString
+	separatedDiacriticalExpression?.enumerateMatchesInString(string.string, options: nil, range: NSRange(string)) { result, _, _ in
+		if result.rangeAtIndex(2).length > 0 {
+			string.replaceCharactersInRange(result.rangeAtIndex(2), withString: (string.string as NSString).substringWithRange(result.rangeAtIndex(2)) + "\u{0308}")
+			string.replaceCharactersInRange(result.rangeAtIndex(1), withString: "")
+		} else {
+			string.replaceCharactersInRange(result.range, withString: "\u{0308}")
+		}
+	}
 	return string
 }
 
